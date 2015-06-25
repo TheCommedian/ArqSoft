7c69bb2799d369033455b64b2061f1f6b888bd1e
mbarbere <mbarbere@adinet.com.uy>
2015-06-25 19:54:29 -0300

    Modificaciones a la persistencia y llamado a la misma en SourceCatalogBean

--- source/TurnsPOC/TurnsPOC-ejb/src/java/core/SourceCatalogBean.java
+++ source/TurnsPOC/TurnsPOC-ejb/src/java/core/SourceCatalogBean.java
@@ -11,7 +11,6 @@ import data.DataFacade;
 import data.TurnSourceEntity;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 import javax.ejb.Singleton;
